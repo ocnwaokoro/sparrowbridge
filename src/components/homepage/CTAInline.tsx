a81054780ee2035b016c268fe3fb3c77c167f70e
ocnwaokoro <chibuikem.nwaokoro@gmail.com>
2026-02-28 11:46:45 -0500

    style(CTAInline): center CTA text and button

--- src/components/homepage/CTAInline.tsx
+++ src/components/homepage/CTAInline.tsx
@@ -16,7 +16,7 @@ export const CTAInline: React.FC<CTAInlineProps> = ({
 }) => {
   return (
     <div className={`w-full max-w-[1000px] p-4 bg-P border border-P/20 rounded-btn 
-      flex flex-col md:flex-row md:items-center md:justify-between gap-5 text-center md:text-left ${className}`}>
+      flex flex-col md:flex-row md:items-center md:justify-center gap-5 text-center ${className}`}>
       <span className="font-h text-[18px] font-extrabold italic text-white">
         {text}
       </span>
